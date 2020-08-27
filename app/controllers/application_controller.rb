56df5284349e0556982da7531d9bbec06410d217
Angela Guardia <angelaguardia20@gmail.com>
2020-08-26 19:40:33 -0700

    Clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  # before_action :get_number_of_favorites
-
-  # def get_number_of_favorites
-  #   @number_of_favorites = Favorite.all
-  #    # session[:favorites].count
-  # end
+  
 end
