87a977b5aeccd791ee9079ca34bb06a74a211091
Angela Guardia <angelaguardia20@gmail.com>
2020-08-29 09:51:42 -0700

    Create test to ensure a pet can be removed from favorites and cannot be favorited more than once

--- spec/features/favorites/destroy_spec.rb
+++ spec/features/favorites/destroy_spec.rb
@@ -19,20 +19,7 @@ describe 'Remove a Favorite' do
 
     expect(current_path).to eq("/pets/#{@pet.id}")
     expect(page).to have_content("You have removed #{@pet.name} from your favorites!")
-    expect(page).to have_content("My Favorites: 0")
     expect(page).to have_link("Favorite Me!")
+    expect(page).to have_button("My Favorites: 0")
   end
 end
-
-# User Story 12, Can't Favorite a Pet More Than Once
-#
-# As a visitor
-# After I've favorited a pet
-# When I visit that pet's show page
-# I no longer see a link to favorite that pet
-# But I see a link to remove that pet from my favorites
-# When I click that link
-# A delete request is sent to "/favorites/:pet_id"
-# And I'm redirected back to that pets show page where I can see a flash message indicating that the pet was removed from my favorites
-# And I can now see a link to favorite that pet
-# And I also see that my favorites indicator has decremented by 1
