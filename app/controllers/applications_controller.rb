d132357e9fdf881c0b94da554b446cba084afe41
Judith Pillado <judith.pillado@gmail.com>
2020-08-29 22:19:09 -0500

    Finish User Story 16. Add create method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,4 +5,21 @@ class ApplicationsController < ApplicationController
     @favorite_pets = favorite.pets
   end
 
+  def create
+    @application = Applications.new(application_params)
+    @pet_ids = params[:favorite][:pet_id].reject!(&:empty?)
+    @pets = @pet_ids.map {|id| Pet.find(id)}
+
+    @application.save
+    @pets.each {|pet| ApplicationsPet.create(applications: @application, pet: @pet)}
+    flash[:notice] = "Submission complete. You're one step closer to becoming a pet owner!"
+    redirect_to '/favorites'
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
+  end
+
 end
