31b760ad92c2dbbabc04f4beac7f33b3e40b249d
Angela Guardia <angelaguardia20@gmail.com>
2020-08-30 12:52:49 -0700

    Correct pluralization in tests

--- spec/models/applications_spec.rb
+++ spec/models/applications_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Applications do
+describe Application do
 
   describe "validations" do
     it { should validate_presence_of :name }
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -4,6 +4,6 @@ describe PetApplication do
 
   describe "relationships" do
     it { should belong_to :pet }
-    it { should belong_to :applications }
+    it { should belong_to :application }
   end
 end
