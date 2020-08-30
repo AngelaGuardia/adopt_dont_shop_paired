2eab344a725d292e1f01ecdb7d5d5defc70eae34
Judith Pillado <judith.pillado@gmail.com>
2020-08-29 22:11:59 -0500

    Add route to create action

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,5 @@ Rails.application.routes.draw do
   delete '/favorites', to: 'favorites#destroy'
 
   get '/applications/new', to: 'applications#new'
+  post '/applications', to: 'applications#create'
 end
